f1450469e10789302cab6433f5b89eb46ccec714
Lozano1-cell <williamlozano634@gmail.com>
2026-07-30 18:15:13 -0600

    Add age verification for voting eligibility

---
+++ verificar_edad.py
@@ -0,0 +1,7 @@
+ Verificar edad para votar
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad, puedes votar.")
+else:
+    print("No eres mayor de edad, aún no puedes votar.")
