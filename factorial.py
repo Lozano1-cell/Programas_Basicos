1e2ca2ab9bd280435aa5e38e160e6489f04f8ed5
Lozano1-cell <williamlozano634@gmail.com>
2026-07-30 19:41:51 -0600

    Add factorial calculation script in Python

---
+++ factorial.py
@@ -0,0 +1,8 @@
+#  Factorial de un número
+n = int(input("Ingresa un número entero no negativo: "))
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i
+
+print(f"El factorial de {n} es: {factorial}")
