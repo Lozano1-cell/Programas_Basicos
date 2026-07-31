089c44faac0b3748ad5edd15d6e7f110ea02e293
Lozano1-cell <williamlozano634@gmail.com>
2026-07-30 18:28:44 -0600

    Add grading evaluation script with letter grades

---
+++ Evaluación _de _calificaciones con letras.py
@@ -0,0 +1,13 @@
+ Evaluación de calificaciones con letras
+calif = float(input("Ingresa tu calificación numérica (0-100): "))
+
+if calif >= 90:
+    print("Calificación: A")
+elif calif >= 80:
+    print("Calificación: B")
+elif calif >= 70:
+    print("Calificación: C")
+elif calif >= 60:
+    print("Calificación: D")
+else:
+    print("Calificación: F")
